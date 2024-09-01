3f8f63271fb88597d86bcda1c02d9e9165815b08
maicol <maicolmike@hotmail.com>
2024-08-31 23:06:59 -0500

    angular

--- 3-framework/angular/platzi/todoapp/src/app/app.component.ts
+++ 3-framework/angular/platzi/todoapp/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa CommonModule aquí
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // Agrega CommonModule aquí
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todoapp';
+  welcome = 'hola mundo p';
+  task = [
+    'instalar',
+    'proyecto',
+    'componente'
+  ]
 }
